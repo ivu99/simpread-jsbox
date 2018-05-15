98b31af76e98079602a5b0c68bd447260c416b10
Kenshin <fxblog@gmail.com>
2018-05-15 16:36:37 +0800

    Update notify to 2.0.1 version.

--- scripts/vender.js
+++ scripts/vender.js
@@ -3,7 +3,7 @@
 
 // https://github.com/kenshin/notify
 var Notify = ( function () {
-    var VERSION = "2.0.0",
+    var VERSION = "2.0.1",
         name    = "notify",
         root    = "notify-gp",
         roottmpl= "<" + root + ">",
@@ -17,16 +17,29 @@ var Notify = ( function () {
             modal    : "modal",
             snackbar : "snackbar",
         },
+        STATE   = {
+            loading  : "loading",
+            holdon   : "holdon",
+        },
+        POSITION= {
+            lefttop     : "lt",
+            leftbottom  : "lb",
+            rightbottom : "rb",
+        },
         options = {
             version : VERSION,
             title   : "",
             content : "",
             type    : NORMAL,
             mode    : MODE.toast,
+            state   : undefined,
+            flat    : false,
             delay   : 1000 * 5,
             icon    : "",
             action  : "",
+            cancel  : "",
             callback: undefined,
+            complete: undefined,
         },
         timer      = {},
         $root,
@@ -37,8 +50,15 @@ var Notify = ( function () {
             <notify-title></notify-title>\
             <notify-content></notify-content>\
             <notify-action></notify-action>\
+            <notify-cancel></notify-cancel>\
         </notify>',
-        prefix      = function( value ) {
+        loading    = '\
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling">\
+                <circle stroke="#fff" stroke-width="10" cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" r="30" stroke-dasharray="141.37166941154067 49.12388980384689" transform="rotate(102 50 50)">\
+                    <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform>\
+                </circle>\
+            </svg>',
+        prefix     = function( value ) {
             return name + "-" + value;
         },
         registyElement = function( name, elements ) {
@@ -56,9 +76,12 @@ var Notify = ( function () {
             hidden( this );
         },
         callbackHander = function( event ) {
-            event.data[1]();
+            event.data[1] && event.data[1]( event.data[2] );
             $root.off( "click", "." + event.data[0] + " notify-action", callbackHander );
             hidden( $(this).parent() );
+        },
+        completeHandler = function() {
+            hidden( this );
         },
         hidden = function( target ) {
             target.addClass( "notify-hide" ).slideUp( 500, function() {
@@ -73,7 +96,9 @@ var Notify = ( function () {
                 $close   = $target.find(prefix( "a"       )),
                 $icon    = $target.find(prefix( "i"       )),
                 $action  = $target.find(prefix( "action"  )),
-                item     = "notify-item-" + num++;
+                $cancel  = $target.find(prefix( "cancel"  )),
+                item     = "notify-item-" + num++,
+                position = this.constructor.Position;
 
             this.title   ? $title.text( this.title )     : $title.hide();
             this.content ? $content.html( this.content ) : $content.hide();
@@ -105,11 +130,42 @@ var Notify = ( function () {
             if ( this.action !== "" && this.callback && typeof this.callback == "function" ) {
                 $content.css( "width", "100%" );
                 $action.text( this.action ).css( "display", "block" );
-                $root.on( "click", "." + item + " notify-action", [ item, this.callback ], callbackHander );
+                $root.on( "click", "." + item + " notify-action", [ item, this.callback, "action" ], callbackHander );
             }
 
-            this.mode !== MODE.modal && ( this.action == "" || !this.callback || typeof this.callback != "function" ) &&
+            if ( this.cancel !== "" && this.callback && typeof this.callback == "function" ) {
+                $content.css( "width", "100%" );
+                $cancel.text( this.cancel ).css( "display", "block" );
+                $root.on( "click", "." + item + " notify-cancel", [ item, this.callback, "cancel" ], callbackHander );
+            }
+
+            this.mode !== MODE.modal && this.state !== STATE.loading && this.state !== STATE.holdon && ( this.action == "" || !this.callback || typeof this.callback != "function" ) &&
                 ( timer[item] = setTimeout( delayHandler.bind( $target, item ), this.delay ) );
+
+            if ( this.state == STATE.loading ) {
+                $icon.html( loading );
+                $icon.css({ display: "block" });
+                this.complete = completeHandler.bind( $target );
+            }
+
+            if ( this.state == STATE.holdon ) {
+                $icon.css({ display: "block" }).addClass( "holdon" );
+                $cancel.after( $icon[0].outerHTML );
+                $target.find( "notify-i:first" ).remove();
+                $root.on( "click", "." + item + " notify-i", [ item, this.callback, "holdon" ], callbackHander );
+                if ( !this.action || !this.cancel ) $content.css({ width: "100%" });
+            }
+
+            if ( this.flat ) {
+                $target.css({ "box-shadow": "none", "border-radius": "2px" });
+            }
+
+            if ( position == POSITION.rightbottom || position == POSITION.leftbottom ) {
+                $target.css({ "transform-origin": "left bottom 0px" });
+                $root.addClass( "notify-position-" + position + "-corner" );
+            } else if ( position == POSITION.lefttop ) {
+                $root.addClass( "notify-position-" + position + "-corner" );
+            }
 
             $target.addClass( item );
             $root.append( $target ).css( "z-index", 2147483647 );
@@ -129,10 +185,15 @@ var Notify = ( function () {
     Notify.prototype.content = options.content;
     Notify.prototype.type    = options.type;
     Notify.prototype.mode    = options.mode;
+    Notify.prototype.state   = options.state;
     Notify.prototype.delay   = options.delay;
     Notify.prototype.icon    = options.icon;
+    Notify.prototype.flat    = options.flat;
     Notify.prototype.action  = options.action;
+    Notify.prototype.cancel  = options.cancel;
     Notify.prototype.callback= options.callback;
+    Notify.prototype.complete= options.complete;
+    Notify.Position          = undefined;
 
     Notify.prototype.Render  = function () {
 
@@ -183,6 +244,7 @@ var Notify = ( function () {
         else {
             console.error( "Arguments error", arguments );
         }
+        return self;
     };
 
     Notify.prototype.Clone  = function () {
