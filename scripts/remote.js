612385393330f813bf3f7d8ada077d3ab4be6957
Kenshin <fxblog@gmail.com>
2018-05-16 10:42:52 +0800

    Update remote js file path.

--- scripts/remote.js
+++ scripts/remote.js
@@ -2,6 +2,6 @@
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.onload = function() {};
-    script.src = "https://greasyfork.org/scripts/367654-simpread-little/code/SimpRead%20Little.js?version=596860";
+    script.src = "https://greasyfork.org/scripts/367654-simpread-little/code/SimpRead%20Little.js";
     document.getElementsByTagName("head")[0].appendChild(script)
 })();
